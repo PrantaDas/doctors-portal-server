c5d5bc51c5f5a99b16c138e87a712856c43c4db4
PrantaDas <prantodas043@gmail.com>
2022-05-17 21:22:55 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -10,6 +11,23 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hlrbv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    try {
+        await client.connect();
+        console.log('db is connected');
+    }
+
+    finally {
+
+    }
+}
+
+
 app.get('/', (req, res) => {
     res.send('Hello from doctors portal server');
 });
@@ -17,3 +35,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Listening to port', port);
 });
+
+run().catch(console.dir);
